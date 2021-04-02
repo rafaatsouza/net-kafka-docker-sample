e6014ee176a673dc73b4199b0baac3f7bdc25b1b
Rafael Torres <souza.t.rafael@gmail.com>
2021-04-02 15:08:26 -0300

    Adds 'key' to custom exception

--- source/Core/KafkaDockerSample.Core.Domain/Exceptions/Custom/CustomError.cs
+++ source/Core/KafkaDockerSample.Core.Domain/Exceptions/Custom/CustomError.cs
@@ -5,11 +5,16 @@ namespace KafkaDockerSample.Core.Domain.Exceptions.Custom
     public abstract class CustomError
     {
         public HttpStatusCode StatusCode { get; set; }
+
+        public string Key { get; }
+
         public string Message { get; }
 
-        public CustomError(HttpStatusCode statusCode, string error)
+        public CustomError(HttpStatusCode statusCode, 
+            string key, string error)
         {
             StatusCode = statusCode;
+            Key = key;
             Message = error;
         }
     }
